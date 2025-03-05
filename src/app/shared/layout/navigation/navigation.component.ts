f4f23ffb491186be5cbb34f3bce37f8f0b5076c1
orianig <oriana.ig016@gmail.com>
2025-03-06 00:11:09 +0100

    feat: add navigation options

--- src/app/shared/layout/navigation/navigation.component.ts
+++ src/app/shared/layout/navigation/navigation.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject, Renderer2, signal } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [
+    MaterialModule, RouterLink, RouterLinkActive,
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
 
+  router = inject(Router);
+  renderer = inject(Renderer2);
+
+  isMenuCollapsed = true;
+  isAppLoaded = false;
+  isUserLoggedIn = true;
+  newNotificationCount = signal<number>(0);
+
+  logout() {
+    this.isUserLoggedIn = false;
+  }
 }
