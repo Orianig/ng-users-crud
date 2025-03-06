8c732ec67d5c0c2f824c11c4b60508c3962f3309
orianig <oriana.ig016@gmail.com>
2025-03-06 23:51:49 +0100

    feat: add edit user service for create & edit to dialog

--- src/app/pages/users/user-edit-dialog/user-edit-dialog.component.ts
+++ src/app/pages/users/user-edit-dialog/user-edit-dialog.component.ts
@@ -4,21 +4,24 @@ import { CdkScrollable } from '@angular/cdk/scrolling';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogTitle, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
 import { User } from '../../../core/models/user.model';
 import { MaterialModule } from '../../../shared/material.module';
+import { UsersService } from '../../../core/services/users.service';
 
 @Component({
-    selector: 'app-user-edit-dialog',
-    templateUrl: 'user-edit-dialog.component.html',
-    styleUrl: 'user-edit-dialog.component.scss',
-    imports: [
-        FormsModule, ReactiveFormsModule,
-        MatDialogTitle, CdkScrollable, MatDialogContent, MatDialogActions, MaterialModule
-    ]
+  selector: 'app-user-edit-dialog',
+  templateUrl: 'user-edit-dialog.component.html',
+  styleUrl: 'user-edit-dialog.component.scss',
+  imports: [
+    FormsModule, ReactiveFormsModule,
+    MatDialogTitle, CdkScrollable, MatDialogContent, MatDialogActions, MaterialModule
+  ]
 })
 export class UserEditDialogComponent {
   dialogRef = inject<MatDialogRef<UserEditDialogComponent>>(MatDialogRef);
   data = inject<{ user: User }>(MAT_DIALOG_DATA);
+  private usersService = inject(UsersService);
   private formBuilder = inject(FormBuilder);
 
+  isNewUser = !this.data?.user;
   userForm = this.formBuilder.group({
     first_name: [this.data?.user?.first_name || '', Validators.required],
     last_name: [this.data?.user?.last_name || '', Validators.required],
@@ -27,9 +30,36 @@ export class UserEditDialogComponent {
   });
 
   onSaveUser() {
-    if (this.userForm.valid) {
-      this.dialogRef.close(this.userForm.value);
+    if (this.userForm.invalid) return;
+    const userData = this.getEditedUser();
+    if (this.isNewUser) {
+      this.usersService.createUser(userData).subscribe({
+        next: (newUser) => this.dialogRef.close(newUser),
+        error: (err) => console.error('Error al crear usuario', err)
+      });
+    } else {
+      const userId = this.data?.user?.id;
+      if (!userId) {
+        console.error('No se puede actualizar: ID de usuario no encontrado');
+        return;
+      }
+      this.usersService.updateUser(userId, userData).subscribe({
+        next: (updatedUser) => this.dialogRef.close(updatedUser),
+        error: (err) => console.error('Error al actualizar usuario', err)
+      });
     }
+  }
+
+  private getEditedUser(): Partial<User> {
+    const formModel = this.userForm.value;
+
+    return {
+      ...(this.data?.user?.id && {id: this.data.user.id}),
+      first_name: formModel.first_name ?? '',
+      last_name: formModel.last_name ?? '',
+      email: formModel.email ?? '',
+      avatar: formModel.avatar ?? ''
+    };
   }
 
   cancel(): void {
